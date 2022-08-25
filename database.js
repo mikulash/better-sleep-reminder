8e5a837d62640b4b91ef2610f160af337c4ca656
Mikulash <heinz.mikulas@gmail.com>
2022-08-25 14:36:23 +0200

    not styled basis of the homescreen

--- database.js
+++ database.js
@@ -1,16 +1,25 @@
 export const data = {
+  // todo change to proper data
   should: [
-    {name: 'view sunlight', from: 0, to: 0},
-    {name: 'hot shower', from: 0, to: 0},
+    {name: 'view sunlight', from: '00:00', to: '24:00'},
+    {name: 'hot shower', from: '00:00', to: '04:00'},
   ],
   can: [
-    {name: 'caffeine', from: 0, to: 0},
-    {name: 'workout', from: 0, to: 0},
-    {name: 'cold shower', from: 0, to: 0},
-    {name: 'drink water', from: 0, to: 0},
+    {name: 'caffeine', from: '00:00', to: '24:00'},
+    {name: 'workout', from: '00:00', to: '22:00'},
+    {name: 'cold shower', from: '00:00', to: '12:00'},
+    {name: 'drink water', from: '00:00', to: '12:00'},
   ],
 };
 
-export const getWhatHeCan = () => {};
-export const getWhatHeShould = () => {};
-export const getWhatHeShouldNot = () => {};
+export const getWhatHeCan = time => {
+  return data.can.filter(item => item.from < time && item.to > time);
+};
+export const getWhatHeShould = time => {
+  return data.should.filter(item => item.from < time && item.to > time);
+};
+export const getWhatHeShouldNot = time => {
+  let result = data.should.filter(item => item.from > time || item.to < time);
+  result.push(...data.can.filter(item => item.from > time || item.to < time));
+  return result;
+};
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,12 +1,28 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-
+import {FlatList, SafeAreaView, Text, View} from 'react-native';
+import {
+  getWhatHeCan,
+  getWhatHeShould,
+  getWhatHeShouldNot,
+} from '../../database';
 const HomeScreen = () => {
   const [time, setTime] = useState(new Date());
   const timeString = time.getHours() + ':' + time.getMinutes();
+  const normalized = time
+    .toLocaleTimeString(navigator.language, {
+      hour: '2-digit',
+      minute: '2-digit',
+    })
+    .slice(0, -3); //remove seconds to be able to compare strings
 
+  const whatHeCan = getWhatHeCan(normalized);
+  const whatHeShouldNot = getWhatHeShouldNot(normalized);
+  const whatHeShould = getWhatHeShould(normalized);
+  const DATA = [whatHeCan, whatHeShould, whatHeShouldNot];
+  console.log(DATA);
+  console.log(whatHeCan);
   useEffect(() => {
-    const interval = setInterval(() => setTime(new Date()), 1000);
+    const interval = setInterval(() => setTime(new Date()), 30000);
     return () => {
       clearInterval(interval);
     };
@@ -14,19 +30,43 @@ const HomeScreen = () => {
   return (
     <SafeAreaView>
       <Text>{timeString}</Text>
-      <ShouldNotItem />
-      <ShouldNotItem />
-      <ShouldNotItem />
-      <ShouldNotItem />
+      <Text>{normalized}</Text>
+      <Text>What he shouldn't do</Text>
+      <FlatList renderItem={renderShouldNotItem} data={whatHeShouldNot} />
+      <Text>What he should do</Text>
+      <FlatList renderItem={renderShouldItem} data={whatHeShould} />
+      <Text>What he can do</Text>
+      <FlatList renderItem={renderCanItem} data={whatHeCan} />
     </SafeAreaView>
   );
 };
 export default HomeScreen;
 
-const ShouldNotItem = () => {
+const renderShouldNotItem = ({item}) => <ShouldNotItem item={item} />;
+const renderShouldItem = ({item}) => <ShouldItem item={item} />;
+const renderCanItem = ({item}) => <CanItem item={item} />;
+
+const ShouldNotItem = props => {
+  console.log('props', props);
   return (
     <View>
-      <Text>Drinking coffee</Text>
+      <Text>{props.item.name}</Text>
+    </View>
+  );
+};
+const ShouldItem = props => {
+  console.log('props', props);
+  return (
+    <View>
+      <Text>{props.item.name}</Text>
+    </View>
+  );
+};
+const CanItem = props => {
+  console.log('props', props);
+  return (
+    <View>
+      <Text>{props.item.name}</Text>
     </View>
   );
 };
